25753d1cfbbcc69f8ea0ffaef29997f1a3e36733
Danil Babkin <danil.babkin.85@mail.ru>
2022-02-16 12:11:16 +0500

    first commit

---
+++ dirty_main.py
@@ -0,0 +1,8 @@
+from application.salary import *
+from application.db.people import *
+from datetime import datetime
+
+if __name__ == '__main__':
+    calculate_salary()
+    get_employees()
+    datetime.today()
